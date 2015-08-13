79b562211c4c3e49a2cbc8b9abfd74cc086d3280
허상훈 <psht410@gmail.com>
2015-08-13 11:10:14 +0900

    A
    A

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D rigid;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+	void FixedUpdate ()
+    {  
+        rigid.velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+	}
+}
